fb7c0147c12daea62e0dc4df2eabdfdd03f70dac
jose diaz <jose7312000@gmail.com>
2021-05-27 16:59:41 -0500

    Modificando colores

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,9 @@ function App() {
 
   const [tarea, setTarea] = useState('');
   const [tareas, setTareas] = useState([]);
+  const [editar, setEditar] = useState(false);
+  const [id, setId] = useState('');
+  
 
   const agregarTarea = e => {
     e.preventDefault();
@@ -26,8 +29,32 @@ function App() {
     const deleteTareas = tareas.filter(item => item.id !== id)
 
     setTareas(deleteTareas)
+    setEditar(false)
   } 
 
+  const editarTarea = item =>{
+    setEditar(true)
+    setTarea(item.nombre)
+    setId(item.id)
+  }
+
+
+  const editarTareas = e =>{
+    e.preventDefault();
+
+    if (!tarea.trim()) {
+      
+      return
+  }
+    const addTareas = tareas.map(item => item.id === id ? {
+      id:id, nombre:tarea} : item)
+
+      setTareas(addTareas)
+      editar(false)
+      setTareas('')
+      setId('')
+  }
+  
   return (
     <div className="conatiner  mt-5">
       <h1 className="text-center">Mis tareas</h1>
@@ -38,18 +65,22 @@ function App() {
           <ul className="list-group">
             {
               tareas.map(item => (
-                <li className="list-group-item" key={item.id}>
+                <li className="list-group-item list-group-item-primary mt-2" key={item.id}>
                   <span>{item.nombre}</span>
                   <button className="btn  btn-danger btn-sm float-right mx-2" onClick={() => eliminarTarea(item.id)}>Eliminar</button>
-                  <button className="btn  btn-warning btn-sm float-right">Editar</button>
+                  <button className="btn  btn-warning btn-sm float-right" onClick={() => editarTarea(item)}>Editar</button>
                 </li>
               ))
             }
           </ul>
         </div>
         <div className="col-4">
-          <h4 className="text-center">Formulario</h4>
-          <form className="mx-4" onSubmit={agregarTarea}>
+          <h2 className="text-center">
+            {
+              editar ? 'Editar tarea' : 'Nueva tarea'  
+            }
+          </h2>
+          <form className="mx-4" onSubmit={editar ? editarTareas : agregarTarea}>
             <input
               type="text"
               placeholder="Escriba una tarea"
@@ -57,7 +88,13 @@ function App() {
               onChange={e => setTarea(e.target.value)}
               value={tarea}
             />
-            <button type="submit" className="btn btn-dark btn-block">Agregar</button>
+            {
+              editar ? (
+                 <button type="submit" className="btn btn-warning btn-block">Editar</button>
+              ):(
+                <button type="submit" className="btn btn-danger btn-block">Agregar</button>
+              )
+            }
           </form>
         </div>
       </div>
